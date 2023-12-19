241aa433ff004ac3b23b1e94d13e6dfb31dcc36d
mac <mac@macs-MacBook-Pro.local>
2023-12-19 16:45:45 +0530

    test

---
+++ Detect.java
@@ -0,0 +1,52 @@
+public class Detect {
+
+    public static void main(string args[]) {
+        char firstLetterOfConsonants = 'b';
+        System.out.println(firstLetterOfConsonants);
+
+        char lastLetterOfVowels = 'u';
+        System.out.println(lastLetterOfVowels);
+
+        char lastLetterOfConsonants = 'z';
+        System.out.println(lastLetterOfConsonants);
+
+        char resultsOfSem = 'P';
+        System.out.println(resultsOfSem);
+
+        resultsOfSem = 'F';
+        System.out.println(resultsOfSem);
+
+        char gradeOfResults = 'S';
+        System.out.println(gradeOfResults);
+
+        gradeOfResults = 'F';
+        System.out.println(gradeOfResults);
+
+        char symbolOfPotassium = 'K';
+        System.out.println(symbolOfPotassium);
+
+        char symbolOfHydrogen = 'H';
+        System.out.println(symbolOfHydrogen);
+
+        char symbolNitrogen = 'N';
+        System.out.println(symbolNitrogen);
+
+        char typeOfPortUsedForAndroid = 'C';
+        System.out.println(typeOfPortUsedForAndroid);
+
+        typeOfPortUsedForAndroid = 'B';
+        System.out.println(typeOfPortUsedForAndroid);
+
+        char sizeOfCloth = 'S';
+        System.out.println(sizeOfCloth);
+
+        sizeOfCloth = 'M';
+        System.out.println(sizeOfCloth);
+
+        char typesOfGender = 'F';
+        System.out.println(typesOfGender);
+
+        typesOfGender = 'M';
+        System.out.println(typesOfGender);
+    }
+}
